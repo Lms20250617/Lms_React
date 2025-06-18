5285f8085a8ba02fd0c95b7045b338498ab7786e
seungwb <seungwb@gmail.com>
2025-06-18 12:06:07 +0900

    refactor/ lecture/list 페이지 구현 완료

--- src/model/Lecture/IList.ts
+++ src/model/Lecture/IList.ts
@@ -1,3 +1,5 @@
+import type { IManagePlan } from './IManagePlan';
+
 export interface IList {
   courseCntPersonnel: number;
   lecEndDate: string;
@@ -7,6 +9,25 @@ export interface IList {
   lecPersonnel: number;
   lecRoomName: string;
   lecStartDate: string;
+}
+
+export interface IListDetail extends IList {
+  insEmail: string;
+  insHp: string;
+  lecContent: string | null;
+  lecDaysCnt: number | null;
+  lecGoal: string | null;
+  lecInstructorId: string;
+  lecRoomId: number | null;
+  lecSectionCnt: number | null;
+  lecSpecifics: string | null;
+  lecWeekPlanList: IManagePlan[];
+  lectureRound: number | null;
+}
+
+export interface IListDetailResponse {
+  isLectureRegistrationAvailable: boolean;
+  lectureDetailValue: IListDetail;
 }
 
 export interface IListResponse {
