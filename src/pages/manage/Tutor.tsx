f805599ca0075d04694afb5c584d38465957b4fc
seungwb <seungwb@gmail.com>
2025-06-20 16:49:51 +0900

    feature/ 강사 관리 페이지 init commit

--- src/pages/manage/Tutor.tsx
+++ src/pages/manage/Tutor.tsx
@@ -1,9 +1,14 @@
 import { ContentBox } from '../../components/common.componets/ContentBox/ContentBox';
+import { TutorSearch } from '../../components/Manage/Tutor/TutorSearch/TutorSearch';
+import { TutorProvider } from '../../provider/manage/TutorProvider';
 
 export const Tutor = () => {
   return (
     <>
-      <ContentBox>강사 관리</ContentBox>
+      <TutorProvider>
+        <ContentBox>강사 관리</ContentBox>
+        <TutorSearch></TutorSearch>
+      </TutorProvider>
     </>
   );
 };
