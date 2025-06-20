c282d3c204484b480ae51a9c30b452835dcf0291
seungwb <seungwb@gmail.com>
2025-06-20 09:25:04 +0900

    feature/ 학생관리 init commit

---
+++ src/pages/manage/Student.tsx
@@ -0,0 +1,9 @@
+import { ContentBox } from '../../components/common.componets/ContentBox/ContentBox';
+
+export const Student = () => {
+  return (
+    <>
+      <ContentBox>학생 관리</ContentBox>
+    </>
+  );
+};
