0abc609a8d96a91e5d83d3d3bd72bcb92412fc5e
yongJun Park <dydwnsl02@naver.com>
2025-06-25 10:57:55 +0900

    add : 검색 기능 구현

--- src/components/User/Resume/ResumeSearch/ResumeSearch.tsx
+++ src/components/User/Resume/ResumeSearch/ResumeSearch.tsx
@@ -8,12 +8,17 @@ interface ILecture {
   lecName: string;
 }
 
+interface IRound {
+  lectureRound: number;
+}
+
 export const ResumeSearch = () => {
   const lecName = useRef<HTMLInputElement>(null);
-  const [lectureStartRound, setLectureStartRound] = useState<string>();
-  const [lectureEndRound, setLectureEndRound] = useState<string>();
+  const [lectureStartAround, setLectureStartAround] = useState<string>();
+  const [lectureEndAround, setLectureEndAround] = useState<string>();
   const [isInitialized, setIsInitialized] = useState<Boolean>(false);
   const [lectures, setLectures] = useState<ILecture[]>([]);
+  const [rounds, setRounds] = useState<IRound[]>([]);
 
   const { setSearchData } = useContext(ResumeContext);
   const [selectedLecName, setSelectedLecName] = useState<string>('');
@@ -27,42 +32,91 @@ export const ResumeSearch = () => {
           setLectures(result);
         });
     };
+
     fetchLectures();
   }, []);
 
+  useEffect(() => {
+    if (!selectedLecName) {
+      setRounds([]);
+      return;
+    }
+
+    const fetchRoundByLecName = async () => {
+      await axios
+        .post(`/api/user/lecture-round-selectbox/${selectedLecName}`)
+        .then((res: AxiosResponse<IRound[]>) => {
+          const result = res.data;
+          setRounds(result);
+        });
+    };
+    fetchRoundByLecName();
+  }, [selectedLecName]);
+
   const handlerSearch = () => {
+    if (
+      lectureStartAround &&
+      lectureEndAround &&
+      Number(lectureEndAround) < Number(lectureStartAround)
+    ) {
+      alert('종료회차는 시작회차보다 빠를 수 없습니다.');
+      return;
+    }
     setSearchData({
-      lecName: selectedLecName,
-      lectureStartRound: lectureStartRound || '',
-      lectureEndRound: lectureEndRound || '',
+      lectureName: selectedLecName,
+      lectureStartAround: lectureStartAround || '',
+      lectureEndAround: lectureEndAround || '',
     });
   };
 
   return (
     <div className="resume-container">
       <div className="input-box">
-        <select
-          className="w-full rounded-sm border border-gray-300 p-2 md:w-56"
-          onChange={(e) => setSelectedLecName(e.target.value)}
-          value={selectedLecName}
-        >
-          <option value="">강의 이름</option>
-          {lectures.map((l) => (
-            <option key={l.lecId} value={l.lecName}>
-              {l.lecName}
-            </option>
-          ))}
-        </select>
-        <select
-          className="w-full rounded-sm border border-gray-300 p-2 md:w-56"
-          onChange={(e) => setLectureStartRound(e.target.value)}
-        >
-          <option value="">회차</option>
-        </select>
-        <select
-          className="w-full rounded-sm border border-gray-300 p-2 md:w-56"
-          onChange={(e) => setLectureEndRound(e.target.value)}
-        ></select>
+        <span>
+          강의 이름
+          <select
+            className="w-full rounded-sm border border-gray-300 p-2 md:w-56"
+            onChange={(e) => setSelectedLecName(e.target.value)}
+            value={selectedLecName}
+          >
+            <option value="">전체</option>
+            {lectures.map((l) => (
+              <option key={l.lecId} value={l.lecName}>
+                {l.lecName}
+              </option>
+            ))}
+          </select>
+        </span>
+        <br />
+        <span>
+          시작 회차
+          <select
+            className="w-full rounded-sm border border-gray-300 p-2 md:w-56"
+            onChange={(e) => setLectureStartAround(e.target.value)}
+          >
+            <option value="">전체</option>
+            {rounds.map((r) => (
+              <option key={r.lectureRound} value={r.lectureRound}>
+                {r.lectureRound}
+              </option>
+            ))}
+          </select>
+        </span>
+        <span>
+          종료 회차
+          <select
+            className="w-full rounded-sm border border-gray-300 p-2 md:w-56"
+            onChange={(e) => setLectureEndAround(e.target.value)}
+          >
+            <option value="">전체</option>
+            {rounds.map((r) => (
+              <option key={r.lectureRound} value={r.lectureRound}>
+                {r.lectureRound}
+              </option>
+            ))}
+          </select>
+        </span>
+
         <button onClick={handlerSearch}>검색</button>
       </div>
     </div>
